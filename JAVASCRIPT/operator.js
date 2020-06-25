145176d5ac36ef5dbe41e92d001b61bbdcedb1b9
gangchul77 <gangchul77@gmail.com>
2020-06-25 14:14:42 +0900

    JavaScript study Ver.1 add operator.js file

--- JAVASCRIPT/operator.js
+++ JAVASCRIPT/operator.js
@@ -40,3 +40,9 @@ let y = 6;
  x -= y;
  x *= y;
  x /= y;
+
+//  5. Comparison operators
+console.log(10 < 6); //less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 8); //greater than
+console.log(10 >= 6); //greater than or equal
