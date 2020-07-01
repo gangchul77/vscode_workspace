4032fd7bc2498c53e8cf51d24747d839c8253f93
gangchul77 <gangchul77@gmail.com>
2020-07-01 21:51:14 +0900

    JavaScript study update callback-to-promise.js file

--- JAVASCRIPT/async/callback-to-promise.js
+++ JAVASCRIPT/async/callback-to-promise.js
@@ -2,6 +2,47 @@
 
 // callback Hell example
 class UserStorage{
+    loginUser(id, password){
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+                if(
+                    ( id === 'steel' && password === 'lee' )||
+                    ( id === 'coder' && password === 'academy')
+                    
+                ) {
+                    resolve(id);
+                }else{
+                    reject(new Error('not found'));
+                }
+            }, 2000);
+        });
+    }
+
+    getRoles(user) {
+       return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(user === 'steel') {
+                    resolve({name: 'steel', role: 'admin'});
+                }else{
+                    reject(new Error('no acccess'));
+                }
+            }, 1000);
+       });
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(id, password)
+.then(userStorage.getRoles)
+.then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+.catch(console.log);
+
+/** 
+// callback Hell example
+class UserStorage{
     loginUser(id, password, onSuccess, onError){
         setTimeout(() => {
             if(
@@ -44,3 +85,5 @@ userStorage.loginUser(id, password,
     },
     (error) => { console.log(error)}    
 );
+
+*/
